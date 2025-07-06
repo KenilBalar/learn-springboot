deca7da4d6367396482b6f559bc91b7746e58261
LetMeet <kenilbalar.kb786@gmail.com>
2025-07-06 17:51:34 +0200

    New Library | New Controller
    - Added PostgreSQL and Spring Data JPA library
    - Added Software Engineer Controller

---
+++ src/main/java/com/ram/learnspringboot/controller/SoftwareEngineerController.java
@@ -0,0 +1,56 @@
+package com.ram.learnspringboot.controller;
+
+import com.ram.learnspringboot.model.SoftwareEngineer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return List.of(
+                new SoftwareEngineer(1,
+                        "Kenil Balar",
+                        "Software Engineer",
+                        "kenil123@gmail.com",
+                        "+49134408425",
+                        "Berlin, Germany",
+                        List.of("Kotlin, Java, Android, Jetpack Compose, Spring-Boot, HTML5, CSS3, JavaScript, SQL, PostgreSQL, Firebase")),
+                new SoftwareEngineer(1,
+                        "Mohit Sukhadiya",
+                        "Front-end Developer",
+                        "mohit543@gmail.com",
+                        "+919573164864",
+                        "Surat, India",
+                        List.of("HTML5, CSS3, JavaScript, Tailwind CSS, React.js")),
+                new SoftwareEngineer(1,
+                        "Jay Patel",
+                        "Back-end Developer",
+                        "jay636@gmail.com",
+                        "+918593165241",
+                        "Delhi, India",
+                        List.of("Java, JavaScript, Spring-Boot, PostgreSQL, Git, Docker")),
+                new SoftwareEngineer(1,
+                        "Sagar Jariwala",
+                        "Android Developer",
+                        "Sagar0435@gmail.com",
+                        "+918866176834",
+                        "Ahmedabad, India",
+                        List.of("Kotlin, Java, Android, Jetpack Compose, Room Database, Firebase")),
+                new SoftwareEngineer(1,
+                        "Piyush Goswami",
+                        ".Net Developer",
+                        "kenil123@gmail.com",
+                        "+919768431846",
+                        "Pune, India",
+                        List.of("C#, C, SQL, Python, Django, AWS, Firebase"))
+        );
+
+    }
+}
---
+++ src/main/java/com/ram/learnspringboot/model/SoftwareEngineer.java
@@ -0,0 +1,94 @@
+package com.ram.learnspringboot.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SoftwareEngineer {
+
+    private Integer id;
+    private String name;
+    private String designation;
+    private String email;
+    private String phone;
+    private String address;
+    private List<String> techStack;
+
+
+    public SoftwareEngineer(Integer id, String name, String designation, String email, String phone, String address, List<String> techStack) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.techStack = techStack;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String surname) {
+        this.designation = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<String> getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(List<String> techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(designation, that.designation) && Objects.equals(email, that.email) && Objects.equals(phone, that.phone) && Objects.equals(address, that.address) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, designation, email, phone, address, techStack);
+    }
+}
