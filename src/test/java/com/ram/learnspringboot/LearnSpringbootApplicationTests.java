b63929269e2bf3b5cbe064709f5bcefe498dd0d4
LetMeet <kenilbalar.kb786@gmail.com>
2025-07-06 15:52:27 +0200

    Initial Commit

---
+++ src/test/java/com/ram/learnspringboot/LearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
